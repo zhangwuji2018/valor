34d5c01f91d048fcae5449315f46ae05f385ad8b
daibing <daibing@foxmail.com>
2019-03-29 13:21:53 +0800

    提交版本：0.0.1 说明：正式开发

---
+++ src/main/java/com/db/valor/ValorApplication.java
@@ -0,0 +1,18 @@
+package com.db.valor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ * @author daibing
+ * @date 2019/2/28
+ */
+@SpringBootApplication
+public class ValorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorApplication.class, args);
+    }
+
+}
